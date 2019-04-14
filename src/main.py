d35cbf957a2aa4ed6a7fba5e4513ac99f1f6e7d6
jason <18867753339@163.com>
2019-04-14 16:37:40 +0800

    bug fixed

--- src/main.py
+++ src/main.py
@@ -29,7 +29,7 @@ def train(model, batch_size, data_handle, weight_decay = 0.0005):
             for x, y in gen():
                 cnt += 1
                 index += 1
-                _, train_loss, train_acc, train_top5_acc = sess.run([train_step, loss, top1_acc, top5_acc], feed_dict = {X: x,Y: y,train: True})
+                _, train_loss, train_acc, train_top5_acc = sess.run([train_step, cross_entropy, top1_acc, top5_acc], feed_dict = {X: x,Y: y,train: True})
                 if cnt % 10 == 0:
                     print("train epoch = {:d}, loss = {:f}, top_1_acc = {:f}, top_5_acc = {:f}".format(epoch, train_loss, train_acc, train_top5_acc))
             if epoch % 5 == 0:
@@ -41,5 +41,6 @@ if __name__ == "__main__":
     Vgg16 = model.Vgg16([None, 32, 32, 3], [None, 100])
     Vgg16.build()
     data_handle = data_manager.Data_Manager("/Users/zhangfucheng/data/cifar-100-python/train", "/Users/zhangfucheng/data/cifar-100-python/test")
+    # data_handle = data_manager.Data_Manager("/home/zhangfucheng/Draft/train", "/home/zhangfucheng/Draft/test")
     train(Vgg16, 128, data_handle)
 
